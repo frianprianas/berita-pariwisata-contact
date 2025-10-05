e251ad9f3593f6d714b4f1481f969825dd770e68
frian <frianp87@protonmail.com>
2025-10-05 19:52:30 +0700

    Update package name to id.smkbaknus666.wartawisata v1.0.4+5

---
+++ android/app/src/main/kotlin/id/smkbaknus666/wartawisata/MainActivity.kt
@@ -0,0 +1,5 @@
+package id.smkbaknus666.wartawisata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/id/smkbn666/beritapariwisata/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package id.smkbn666.beritapariwisata
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
